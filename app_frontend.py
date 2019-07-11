d5b8eed2d80adf4aa074e4a9752d230b5be572d9
joaogracio <joaogojuryu@hotmail.com>
2019-07-11 18:33:31 +0100

    Versão antes da alteração para um novo sistema de base de dados

--- app_frontend.py
+++ app_frontend.py
@@ -72,6 +72,41 @@ def perguntas():
 
     return render_template("/perguntas/perguntas.html", perguntas = perguntas)
 
+@app.route('/v1.0/perguntas/pergunta/<int:pergunta_id>')
+def perguntas_pergunta(pergunta_id):
+    url_inicial = "http://127.0.0.1:80"
+
+    url_final = "/v1.0/perguntas/" + str(pergunta_id)
+
+    pergunta_request = requests.get(url_inicial + url_final)
+
+    pergunta = pergunta_request.json()
+
+    #pergunta_id = pergunta['pergunta_id']
+
+    url_final = "/v1.0/pergunta_has_tabela/" + str(pergunta_id)
+
+    pergunta_has_tabela_request = requests.get(url_inicial + url_final)
+
+    pergunta_has_tabelas = pergunta_has_tabela_request.json()
+
+    print("ANTES DE ENTRAR NO LOOP")
+    print("##################################################")
+    print(pergunta_has_tabelas)
+
+    tabelas = []
+    for each in pergunta_has_tabelas:
+        print("EACH: ", each)
+        url_final = "/v1.0/tabela/" + str(each['tabela_Tabela_id'])
+
+        tabela_request = requests.get(url_inicial + url_final)
+
+        tabela = tabela_request.json()
+
+        tabelas.append(tabela)
+
+    return render_template('/perguntas/pergunta.html', pergunta = pergunta, tabelas= tabelas) 
+
 @app.route('/v1.0/perguntas/delete/<int:pergunta_id>')
 def perguntas_delete_get(pergunta_id):
     
@@ -114,6 +149,42 @@ def respostas():
     respostas = respostas_request.json()
 
     return render_template('/respostas/respostas.html', respostas = respostas)
+
+@app.route('/v1.0/respostas/resposta/<int:resposta_id>')
+def respostas_resposta(resposta_id):
+    url_inicial = "http://127.0.0.1:80"
+
+    url_final = "/v1.0/respostas/" + str(resposta_id)
+
+    resposta_request = requests.get(url_inicial + url_final)
+
+    resposta = resposta_request.json()
+
+    url_final = "/v1.0/resposta_has_tabela/" + str(resposta_id) + "/" + str(resposta['aluno_aluno_id'])+ "/" + str(resposta['pergunta_pergunta_id']) + "/"
+
+    print("respostas_resposta: url_inicial + url_final: ", url_inicial + url_final)
+
+    resposta_has_tabela_request = requests.get(url_inicial + url_final)
+
+    print("respostas_resposta: respostas_has_tabela_request: ", resposta_has_tabela_request)
+    print("respostas_resposta: dir(respostas_has_tabela_request)", dir(resposta_has_tabela_request))
+    print("respostas_resposta: respostas_has_tabela_request.json", resposta_has_tabela_request.json)
+
+
+    resposta_has_tabelas = resposta_has_tabela_request.json()
+
+    tabelas = []
+    for each in resposta_has_tabelas:
+        print("EACH: ", each)
+        url_final = "/v1.0/tabela/" + str(each['tabela_Tabela_id'])
+
+        tabela_request = requests.get(url_inicial + url_final)
+
+        tabela = tabela_request.json()
+
+        tabelas.append(tabela)
+
+    return render_template('/respostas/resposta.html', resposta = resposta, tabelas= tabelas)
 
 @app.route('/v1.0/respostas/insert/')
 def resposta_insert_get():
@@ -185,6 +256,22 @@ def resultado_delete_get(resultado_id):
     resultado = resultado_request.json()
 
     return render_template('/resultados/delete.html', resultado = resultado)
+
+@app.route('/v1.0/tabelas/')
+def tabelas():
+    return 
+
+@app.route('/v1.0/tabela/<int:tabela_id>')
+def tabelas_tabela(tabela_id):
+    url_inicial = "http://127.0.0.1:80"
+
+    url_final_tabela = "/v1.0/tabela/" + str(tabela_id)
+
+    tabela_request = requests.get(url_inicial + url_final_tabela)
+
+    tabela = tabela_request.json()
+
+    return render_template('/tabelas/tabela.html', tabela = tabela)
 
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
